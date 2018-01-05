204abf028cca6725145df491a97faf9646e6639d
Dione Wilson <dionew1@gmail.com>
2018-01-05 09:01:32 -0700

    Setup User model and database

---
+++ db/migrate/20180105011143_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration[5.1]
+  def change
+    enable_extension("citext")
+
+    create_table :users do |t|
+      t.text     :uid
+      t.citext   :first_name
+      t.citext   :last_name
+      t.citext   :email
+      t.text     :token
+      t.datetime :oauth_expires_at
+    end
+  end
+end
