b6c03e911cd566d3c64dbb699e47c8a431c69d95
Dione Wilson <dionew1@gmail.com>
2018-01-04 17:16:01 -0700

    TDD Oauth for Google

---
+++ spec/features/user_can_login_with_google_account_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can login with google account' do
+  it "after clicking on login with google link" do
+    stub_omniauth
+
+    visit root_path
+
+    expect(page).to have_content "Sign in with Google"
+
+    click_on "Sign in with Google"
+
+    expect(page).to have_content "Welcome"
+    expect(page).to have_content "Dione"
+    expect(page).to have_link "Logout"
+  end
+end
