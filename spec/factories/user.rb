85a566de01aae8dba3f3e06506c8bb4ede018827
Dione Wilson <dionew1@gmail.com>
2018-01-06 21:47:22 -0700

    Update test for factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     first_name { Faker::HitchhikersGuideToTheGalaxy.character }
     last_name  { Faker::HitchhikersGuideToTheGalaxy.planet }
     token { Faker::Code.asin }
-    oauth_expires_at { Faker::Time.forward(23, :morning) }
+    oauth_expires_at { Faker::Number.number(6) }
+    google_id { Faker::Number.digit }
     email { Faker::Internet.email }
   end
 end
