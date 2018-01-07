2c4615dd22e058e239ce51780a10a780274445c8
Dione Wilson <dionew1@gmail.com>
2018-01-06 23:54:05 -0700

    Add states to page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  include ApplicationHelper::States
+
+  helper_method :current_user, ApplicationHelper::States.instance_methods
 
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
--- spec/features/user_search_by_state_spec.rb
+++ spec/features/user_search_by_state_spec.rb
@@ -5,7 +5,8 @@ feature "User can see states to search by" do
 
   it "when logged in" do
     allow_any_instance_of(ApplicationController).to receive(:current_user). and_return(user)
-
+    visit root_path
+    
     expect(page).to have_selector(".state")
   end
 end
