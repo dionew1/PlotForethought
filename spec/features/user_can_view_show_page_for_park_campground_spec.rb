6f99017976c7b321bcdf0f6a4c4053928b8332ab
Dione Wilson <dionew1@gmail.com>
2018-01-10 13:20:28 -0700

    Add VCR for testing

--- spec/features/user_can_view_show_page_for_park_campground_spec.rb
+++ spec/features/user_can_view_show_page_for_park_campground_spec.rb
@@ -3,18 +3,20 @@ require 'rails_helper'
 feature 'User can view camping_info' do
   let!(:user) { create(:user) }
 
-  it "selecting a park from index" do
+  it "by selecting a park from index" do
     allow_any_instance_of(ApplicationController).to receive(:current_user). and_return(user)
     visit root_path
-    click_on "ALASKA"
-    click_on "Wonder Lake"
+    VCR.use_cassette("services/user_can_see_camp_info") do
+      click_on "ALASKA"
+      click_on "Wonder Lake"
 
-    expect(page).to have_selector(".name")
-    expect(page).to have_selector(".description")
-    expect(page).to have_selector(".direction_info")
-    expect(page).to have_selector(".contact-info")
-    expect(page).to have_selector(".site-count-summary")
-    expect(page).to have_selector(".amenities")
-    expect(page).to have_selector(".accessibility")
+      expect(page).to have_selector(".name")
+      expect(page).to have_selector(".description")
+      expect(page).to have_selector(".direction_info")
+      expect(page).to have_selector(".contact-info")
+      expect(page).to have_selector(".site-count-summary")
+      expect(page).to have_selector(".amenities")
+      expect(page).to have_selector(".accessibility")
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,19 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'spec_helper'
+require 'rspec/rails'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+end
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
